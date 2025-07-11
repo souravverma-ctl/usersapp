4ac9cca66217cec55a478a44bce4ff9b69a88652
Sourav <souravshine@gmail.com>
2025-07-11 13:40:19 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import {Provider} from "react-redux";
 // import store from './todo-ways/todoReduxStore/store';
 // import store from './todo-ways/todoReduxToolkit/store';
